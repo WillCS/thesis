35cb0f9dec687fc5a2e2c13f1c4c627fa728a7da
WillCS <wcstibbards@gmail.com>
2021-09-02 18:47:49 +1000

    Changed how the random data is generated, should be more realistic now, also gives backbones that are scarily close to what the actual data gives me so I'm gonna have to do some quantitative analysis to see if there really is a difference.

---
+++ scribbles.md
@@ -0,0 +1,18 @@
+# For analysing meaningfulness of data
+## On the genetic data
+- Take a series of backbones at varying p values
+- Fit a power law to the degree sequence of each backbone
+- Plot the exponents of the power laws against the p values
+
+- Plot backbone size against p value
+- Plot backbone strength against p value
+
+## On the random data
+- Generate a sample of random data, then for each sample:
+  - Take a series of backbones at varying p values
+  - Fit a power law to the degree sequence of each backbone
+- Take the average exponent of the power laws for each p value
+- Plot the average exponents of thepower laws against the p values
+
+- Plot average backbone size against p value
+- Plot average backbone strength against p value
--- src/data/random_genetic_data.py
+++ src/data/random_genetic_data.py
@@ -1,26 +1,41 @@
-from math import factorial, sqrt, pi, asin
+from math import sqrt
 import random
-from typing import Optional, List
+from typing import Optional, List, Tuple
+from statistics import mean
 
 import networkx as nx
-import numpy as np
-from scipy.interpolate import interp1d
 
-def generate_weights(n: int) -> List[float]:
-    pdf = lambda x: (4 / pi) * sqrt(1 - pow(x, 2))
+# By the central limit theorem, the sum of independent random variables
+# approximates a normal distribution.
+def get_random_normal(n: int = 3) -> float:
+    return sum([random.uniform(0, 1 / n) for _ in range(n)])
 
-    xs = np.linspace(0, 1, 1000)
-    c  = np.cumsum([pdf(x) for x in xs])
-    c -= c.min()
-    f  = interp1d(c / c.max(), xs)
+def correlation(
+    v: Tuple[float, float, float, float, float],
+    u: Tuple[float, float, float, float, float]
+) -> float:
+    v_mean = mean(v)
+    u_mean = mean(u)
 
-    return f(np.random.random(n))
+    c_v = [v_i - v_mean for v_i in v]
+    c_u = [u_i - u_mean for u_i in u]
+
+    numerator   = sum([c_v[i] * c_u[i] for i in range(5)])
+    denominator = sqrt(sum([pow(c_v_i, 2) for c_v_i in c_v]) * sum([pow(c_u_i, 2) for c_u_i in c_u]))
+
+    return abs(numerator / denominator)
+
+def generate_vertex() -> Tuple[float, float, float, float, float]:
+    return tuple([get_random_normal() for _ in range(5)])
+
+def generate_vertices(n: int) -> List[Tuple[float, float, float, float, float]]:
+    return [generate_vertex() for v in range(n)]
 
 def create_random_complete_graph(n: int, seed: Optional[int] = None) -> nx.Graph:
     if seed is not None:
         random.seed(seed)
 
-    weights = generate_weights(n * (n - 1) // 2)
+    tuples = generate_vertices(n)
 
     graph = nx.Graph()
     graph.add_nodes_from([v for v in range(n)])
@@ -28,7 +43,7 @@ def create_random_complete_graph(n: int, seed: Optional[int] = None) -> nx.Graph
     i = 0
     for v in range(n):
         for u in range(v + 1, n):
-            graph.add_edge(v, u, weight = weights[i])
+            graph.add_edge(v, u, weight = correlation(tuples[v], tuples[u]))
             i = i + 1
 
     return graph
